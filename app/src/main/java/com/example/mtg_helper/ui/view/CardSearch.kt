72bddaaee7e81064d6d20cbb72f3b84ed0e045ce
Álvaro Fernández <75076925+notAvu@users.noreply.github.com>
2023-08-06 16:10:47 +0200

    Update CardSearch.kt

--- app/src/main/java/com/example/mtg_helper/ui/view/CardSearch.kt
+++ app/src/main/java/com/example/mtg_helper/ui/view/CardSearch.kt
@@ -1,12 +1,21 @@
 package com.example.mtg_helper.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-
 import com.example.mtg_helper.presentation.CardSearchVM
 
 @Composable
 fun CardSearch(navController: NavController, cardSearchVM: CardSearchVM = hiltViewModel()) {
-    /*TODO*/
+    val state = cardSearchVM.state.value
+    Column(){
+        
+        Text(text = "Title")
+        Button(onClick = {navController.navigate(/*TODO*/0)}) {
+            Text(text = "Search")
+        }
+    }
 }
